7e2e6c961132ef5c9170fc281893918a87c63d92
Nancy-Juarez <Nancy.Juarez@e2.com>
2026-01-20 11:34:28 -0600

    [feat]: add action UI feature

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 import logo from "./logo.webp";
+import deleteIcon from "./delete.png";
 
-export { logo };
+export { logo, deleteIcon };
--- src/features/reports/views/Reports.tsx
+++ src/features/reports/views/Reports.tsx
@@ -1,74 +1,32 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import clsx from "clsx";
+import { useGetReportsQuery } from "../services/reportApi";
+import type { ReportPriority } from "../types/report.types";
+import { deleteIcon } from "../../../assets";
+
+const getStatusStyles = (status: string) => {
+  switch (status) {
+    case "Completado":
+      return "bg-green-100 text-green-700 border-green-200";
+    case "En Progreso":
+      return "bg-blue-100 text-blue-700 border-blue-200";
+    case "Pendiente":
+      return "bg-yellow-100 text-yellow-700 border-yellow-200";
+    default:
+      return "bg-gray-100 text-gray-700";
+  }
+};
 
 const Periods = () => {
-  // Datos de ejemplo
-  const datos = [
-    {
-      id: 1,
-      asunto: "Actualizar servidores",
-      prioridad: "Alta",
-      fecha: "2024-03-10",
-      estatus: "Completado",
-    },
-    {
-      id: 2,
-      asunto: "Revisión de diseño UI",
-      prioridad: "Media",
-      fecha: "2024-03-12",
-      estatus: "En Progreso",
-    },
-    {
-      id: 3,
-      asunto: "Backup de base de datos",
-      prioridad: "Alta",
-      fecha: "2024-03-15",
-      estatus: "Pendiente",
-    },
-    {
-      id: 4,
-      asunto: "Reporte de métricas Q1",
-      prioridad: "Baja",
-      fecha: "2024-03-18",
-      estatus: "Completado",
-    },
-    {
-      id: 5,
-      asunto: "Fix bug login móvil",
-      prioridad: "Alta",
-      fecha: "2024-03-20",
-      estatus: "En Progreso",
-    },
-    {
-      id: 6,
-      asunto: "Documentación API",
-      prioridad: "Media",
-      fecha: "2024-03-22",
-      estatus: "Pendiente",
-    },
-  ];
+  const { data: reports = [] } = useGetReportsQuery();
 
-  // Lógica de paginación
-  const [paginaActual, setPaginaActual] = useState(1);
-  const filasPorPagina = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowByPage = 2;
 
-  const indiceUltimoItem = paginaActual * filasPorPagina;
-  const indicePrimerItem = indiceUltimoItem - filasPorPagina;
-  const itemsActuales = datos.slice(indicePrimerItem, indiceUltimoItem);
-  const totalPaginas = Math.ceil(datos.length / filasPorPagina);
-
-  // Estilos para Estatus
-  const getEstatusStyle = (estatus: string) => {
-    switch (estatus) {
-      case "Completado":
-        return "bg-green-100 text-green-700 border-green-200";
-      case "En Progreso":
-        return "bg-blue-100 text-blue-700 border-blue-200";
-      case "Pendiente":
-        return "bg-yellow-100 text-yellow-700 border-yellow-200";
-      default:
-        return "bg-gray-100 text-gray-700";
-    }
-  };
+  const lastItem = currentPage * rowByPage;
+  const firstItem = lastItem - rowByPage;
+  const currentReports = reports.slice(firstItem, lastItem);
+  const totalPages = Math.ceil(reports.length / rowByPage);
 
   return (
     <>
@@ -94,59 +52,61 @@ const Periods = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100">
-            {itemsActuales.map((item) => (
-              <tr key={item.id} className="hover:bg-gray-50 transition-colors">
-                <td className="px-6 py-4 text-sm font-medium text-gray-900">
-                  {item.asunto}
-                </td>
-                <td className="px-6 py-4 text-sm text-gray-600">
-                  <span
-                    className={`px-2 py-1 rounded-md text-xs font-bold ${item.prioridad === "Alta" ? "text-red-600" : "text-gray-500"}`}
-                  >
-                    {item.prioridad}
-                  </span>
-                </td>
-                <td className="px-6 py-4 text-sm text-gray-500">
-                  {item.fecha}
-                </td>
-                <td className="px-6 py-4">
-                  <span
-                    className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium border ${getEstatusStyle(item.estatus)}`}
-                  >
-                    {item.estatus}
-                  </span>
-                </td>
-                <td className="px-6 py-4 text-sm text-gray-500">
-                  <button className="p-1 hover:bg-gray-200 rounded-lg transition-colors"></button>
-                </td>
-              </tr>
-            ))}
+            {currentReports.map(
+              ({ id, subject, priority, createdAt, status }) => (
+                <tr key={id} className="hover:bg-gray-50 transition-colors">
+                  <td className="px-6 py-4 text-sm font-medium text-gray-900">
+                    {subject}
+                  </td>
+                  <td className="px-6 py-4 text-sm text-gray-600">
+                    <span
+                      className={clsx(
+                        "px-2 py-1 rounded-md text-xs font-bold",
+                        priority === "alta" ? "text-red-600" : "text-gray-500",
+                      )}
+                    >
+                      {priority as ReportPriority}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4 text-sm text-gray-500">
+                    {createdAt}
+                  </td>
+                  <td className="px-6 py-4">
+                    <span
+                      className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium border ${getStatusStyles(status)}`}
+                    >
+                      {status}
+                    </span>
+                  </td>
+                  <td className="flex justify-between items-center pl-6 pr-4 py-4 text-sm text-gray-500">
+                    <button className="mr-8 font-medium text-sm text-blue-primary hover:text-blue-primary cursor-pointer">
+                      Ver detalle
+                    </button>
+                    <button className="p-1 hover:bg-gray-200 rounded-b-sm transition-colors cursor-pointer">
+                      <img src={deleteIcon} alt="delete pending task" />
+                    </button>
+                  </td>
+                </tr>
+              ),
+            )}
           </tbody>
         </table>
       </div>
 
-      <div className="flex items-center justify-between px-6 py-4 bg-gray-50/50 border-t border-gray-200 rounded-b-xl">
-        <p className="text-sm text-gray-600">
-          Mostrando{" "}
-          <span className="font-semibold">{indicePrimerItem + 1}</span> a{" "}
-          <span className="font-semibold">
-            {Math.min(indiceUltimoItem, datos.length)}
-          </span>{" "}
-          de <span className="font-semibold">{datos.length}</span> resultados
-        </p>
+      <div className="flex items-center justify-end px-6 py-4 bg-gray-50/50 border-t border-gray-200 rounded-b-xl">
         <div className="flex space-x-2">
           <button
-            onClick={() => setPaginaActual((prev) => Math.max(prev - 1, 1))}
-            disabled={paginaActual === 1}
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
             className="flex items-center px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed transition-all"
           >
             Previous
           </button>
           <button
             onClick={() =>
-              setPaginaActual((prev) => Math.min(prev + 1, totalPaginas))
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
             }
-            disabled={paginaActual === totalPaginas}
+            disabled={currentPage === totalPages}
             className="flex items-center px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed transition-all"
           >
             Next
