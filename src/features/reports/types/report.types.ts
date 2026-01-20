58cbd75994419dafa3093b17bc1fd833eb50c48c
Nancy-Juarez <Nancy.Juarez@e2.com>
2026-01-20 10:07:58 -0600

    [feat]: update report.types files

--- src/features/reports/types/report.types.ts
+++ src/features/reports/types/report.types.ts
@@ -1,10 +1,11 @@
-export type ReportPriority = "low" | "medium" | "high";
-export type ReportStatus = "open" | "in-progress" | "closed";
+export type ReportPriority = "baja" | "media" | "alta";
+export type ReportStatus = "completado" | "enProgreso" | "pendiente";
 
 export type ReportType = {
   id: string;
   subject: string;
   priority: ReportPriority;
+  detail: string;
   attachment?: string;
   createdAt: string;
   status: ReportStatus;
@@ -14,4 +15,11 @@ export type ViewsType = "reportList" | "create" | "detail";
 
 export type ViewType = {
   currentView: ViewsType;
+};
+
+export type FormTypes = {
+  headTitle: string;
+  initialValues: ReportType;
+  onSubmit: (values: ReportType) => void;
+  onCancel: () => void;
 };
